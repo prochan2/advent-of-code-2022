cba8dec61ba27261d54e57a8c07b9c4f0352824b
Antonín Procházka <antonin@postsharp.net>
2022-12-04 19:56:20 +0100

    Day 1 Task 1

--- src/day1/task1/Program.cs
+++ src/day1/task1/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Immutable;
+
+//const string inputFile = @"..\..\..\input\sinput.txt";
+const string inputFile = @"..\..\..\input\input.txt";
+
+List<Elf> elfs = new();
+
+Elf currentElf = new Elf();
+
+foreach (var line in File.ReadLines(inputFile))
+{
+    if (line == "")
+    {
+        elfs.Add(currentElf);
+        currentElf = new Elf();
+    }
+    else
+    {
+        currentElf = currentElf.WithMeal(new Meal(long.Parse(line)));
+    }
+}
+
+Elf elfWithMostCalories = elfs.MaxBy(e => e.TotalCalories);
+
+Console.WriteLine(elfWithMostCalories.TotalCalories);
+
+readonly struct Elf
+{
+    public Elf()
+    {
+    }
+
+    public IImmutableList<Meal> Meals { get; init; } = ImmutableList<Meal>.Empty;
+
+    public long TotalCalories => this.Meals.Sum(m => m.Calories);
+
+    public Elf WithMeal(Meal meal) => new() { Meals = this.Meals.Add(meal) };
+}
+
+record Meal(long Calories);
